69c12522e5f9d5336cbd52657dd413c8feb70918
NickDallAgnol <nick.dallagnol@gmail.com>
2025-09-29 13:12:48 -0300

    Autenticação das Routes

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,13 @@
+// api/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilita o CORS para permitir requisições do nosso frontend
+  app.enableCors(); // <-- ESSA É A LINHA DA SOLUÇÃO
+
+  await app.listen(3000);
 }
 bootstrap();
