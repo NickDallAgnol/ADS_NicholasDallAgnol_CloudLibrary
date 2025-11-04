63b378394df5823dbecdbf31c01a42e1cfa97c33
NickDallAgnol <nick.dallagnol@gmail.com>
2025-11-04 15:21:40 -0300

    Ajustes no fluxo de registro e validação + Funcionamento do Front

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from './guards/jwt/jwt-auth.guard';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 import { Request as ExpressRequest } from 'express';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() dto: CreateUserDto) {
+    return this.authService.register(dto);
+  }
+
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req: ExpressRequest) {
