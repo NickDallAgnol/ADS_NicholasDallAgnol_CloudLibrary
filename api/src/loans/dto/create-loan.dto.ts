478fe87a2c3a955c40d5640e7561b1f288f0ef66
NickDallAgnol <nick.dallagnol@gmail.com>
2025-11-17 11:55:30 -0300

    Adicionar validadores ao CreateLoanDto (IsNumber, IsNotEmpty)

--- api/src/loans/dto/create-loan.dto.ts
+++ api/src/loans/dto/create-loan.dto.ts
@@ -1,4 +1,11 @@
+import { IsNumber, IsNotEmpty } from 'class-validator';
+
 export class CreateLoanDto {
+  @IsNumber()
+  @IsNotEmpty()
   book_id!: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   borrowed_from_id!: number;
 }
